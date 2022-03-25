008e2385aae0fe68ca1e8373069155316bc85807
Mohammad Nikzad <nikzad.engineer@gmail.com>
2022-03-26 01:39:12 +0430

    first commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Mohammad on 2/25/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+}
